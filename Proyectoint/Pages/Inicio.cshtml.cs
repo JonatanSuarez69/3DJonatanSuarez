7e75b2d1a5aeef605a426091e6811732c8c67db8
jonatanjim3n3z@gmail.com <jonatanjim3n3z@gmail.com>
2020-06-08 09:34:25 -0500

    Version1

---
+++ Proyectoint/Pages/Inicio.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyectoint.Pages
+{
+    public class InicioModel : PageModel
+    {
+        [BindProperty]
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+
+
+        public void OnGet()
+        {
+        }
+    }
+}
