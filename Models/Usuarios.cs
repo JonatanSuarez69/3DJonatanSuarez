44b72b16451b8c766c6d0e8079cb38a5f03d9c95
jonatanjim3n3z@gmail.com <jonatanjim3n3z@gmail.com>
2020-06-16 00:30:30 -0500

    Version 3

---
+++ Models/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("Usuarios", Schema = "dbo")]
+    public class Usuarios
+    {
+        [Key]
+        public int Id_Usuarios { get; set; }
+        [Display(Name="Nombre de usuario")]
+        public string Nombre_Usuario { get; set; }
+        [Display(Name = "Contraseña")]
+        [DataType(DataType.Password)]
+        public string Contraseña { get; set; }
+
+
+    }
+}
---
+++ Proyectoint/Pages/UsuariosController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyectoint.Pages
+{
+    public class UsuariosController : Controller
+    {
+        [HttpPost]
+
+        public IActionResult getUsuarios(String Nombre_Usuario, string Contraseña)
+        {
+
+            var Usuario = context.UsuariosdbSet.Where(s => s.Nombre_Usuarios == Nombre_Usuario && s.Contraseña == Contraseña);
+
+            if (Usuario.Any())
+            {
+                if (Usuario.Where(s => s.Nombre_Usuarios == Nombre_Usuario && s.Contraseña == Contraseña).Any())
+                {
+                    return Json(new { Status = true, message = "Bienvenido" });
+                }
+                else
+                {
+                    return Json(new { Status = false, message = "Contraseña incorrecta" });
+                }
+
+
+            }
+            else
+            {
+                return Json(new { Status = true, message = "Usuario incorrecta" });
+            }
+        }
+    }
+}
