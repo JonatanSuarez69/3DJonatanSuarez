449f44b4cbdf15666447ae370e9eb2b84afdcdec
jonatanjim3n3z@gmail.com <jonatanjim3n3z@gmail.com>
2020-07-30 23:08:59 -0500

    Version final

---
+++ Proyectoint/Reporte/RepCandi.cs
@@ -0,0 +1,185 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Proyectoint.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyectoint.Reporte
+{
+ 
+    public class RepCandi
+    {
+
+        #region Declarion
+        int _maxColumn = 8;
+        Document _document;
+        PdfPTable _pdfTable = new PdfPTable(8);
+        PdfPCell _pdfCell;
+        Font _fontStyle;
+        MemoryStream _memoryStream = new MemoryStream();
+        List<Candi> _oCandi = new List<Candi>();
+        #endregion
+
+
+        public byte[] Report(List<Candi> oCandi )
+        {
+            _oCandi = oCandi;
+
+            _document = new Document(PageSize.A4, 10f, 10f, 20f, 30f);
+            _pdfTable.WidthPercentage = 100;
+            _pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            PdfWriter.GetInstance(_document, _memoryStream);
+            _document.Open();
+
+            float[] sizes = new float[_maxColumn];
+            for(var i=0;i<_maxColumn; i++)
+            {
+                if (i == 0) sizes[i] = 50;
+                else sizes[i] = 100;
+
+            }
+
+            _pdfTable.SetWidths(sizes);
+
+
+            this.ReportHeader();
+            this.ReportBody();
+
+            _pdfTable.HeaderRows = 2;
+            _document.Add(_pdfTable);
+            _document.Close();
+            return _memoryStream.ToArray();
+        }
+
+        private void ReportHeader()
+        {
+            _fontStyle = FontFactory.GetFont("Tahoma", 18f, 1);
+            _pdfCell = new PdfPCell(new Phrase("Nombre del canidato", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+
+
+            _fontStyle = FontFactory.GetFont("Tahoma", 18f, 1);
+            _pdfCell = new PdfPCell(new Phrase("Informacion del candidato", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+
+        }
+
+        private void ReportBody()
+        {
+            _fontStyle = FontFactory.GetFont("Tahoma", 9f, 1);
+            var fontStyle = FontFactory.GetFont("Tahoma", 18f, 0);
+
+            #region Table Header
+            _pdfCell = new PdfPCell(new Phrase("Nombre del candidato", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Sexo", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Cumpleaños", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Estado civil", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Nacionalidad", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Curp", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Referencia", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfTable.CompleteRow();
+            #endregion
+
+            #region
+            int nSL = 1;
+            foreach(var oCandi in _oCandi)
+            {
+                _pdfCell = new PdfPCell(new Phrase(nSL ++.ToString(), fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(oCandi.Sexo, fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase("Cumpleaños", fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(oCandi.Est_civ, fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(oCandi.Nacionalidad, fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(oCandi.Curp, fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(oCandi.Referencia, fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+
+                _pdfTable.CompleteRow();
+
+            }
+            #endregion
+        }
+    }
+}
